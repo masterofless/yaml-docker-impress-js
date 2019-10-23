c480a0cecd95fc17058a3698fab595cd5ab70685
Andy Cohan <cohana@avalonconsult.com>
2019-10-23 09:10:58 -0400

    changed build_run to use default browser

--- build_run.sh
+++ build_run.sh
@@ -12,6 +12,6 @@ docker build -t $tag .
 
 docker run --detach --rm --name $tag -p ${hostport}:80 $tag && sleep 1
 
-open -a Firefox http://localhost:$hostport/presos/ || true
+open http://localhost:$hostport/presos/ || true
 
 docker logs $tag
